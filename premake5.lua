9179e0ce90c1acd4d47fe09e87001eed00499b30
shearrs <114428374+shearrs@users.noreply.github.com>
2024-11-08 10:16:29 -0600

    premake

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "spdlog"
+	kind "StaticLib"
+	language "C++"
+	warnings "off"
+
+	targetdir("bin/" .. outputdir .. "/%{prj.name}")
+	objdir("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/**.h",
+		"src/**.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "on"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+		symbols "off"
